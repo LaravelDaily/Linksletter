fcf04c2561996f880fcdb9f8007e9fd42105349d
PovilasKorop <povilask007@gmail.com>
2024-11-14 11:01:09 +0200

    Links and Issues migrations/models

---
+++ app/Models/Issue.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Issue extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'subject', 'header_text', 'footer_text', 'links_html', 'sent_at', 'user_id'
+    ];
+
+    public function links(): HasMany
+    {
+        return $this->hasMany(Link::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/Link.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Link extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'url', 'title', 'description', 'issue_id', 'position', 'user_id'
+    ];
+
+    public function issue(): BelongsTo
+    {
+        return $this->belongsTo(Issue::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
