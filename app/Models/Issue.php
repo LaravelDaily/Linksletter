25a8db75682f7ea27743c61f50fcd33b51a90375
modestasv <info@modestasv.lt>
2024-11-19 12:47:17 +0200

    Larastan Level 9 run - resolves #7

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Database\Factories\IssueFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,17 +10,24 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Issue extends Model
 {
+    /** @use HasFactory<IssueFactory> */
     use HasFactory;
 
     protected $fillable = [
         'subject', 'header_text', 'footer_text', 'links_html', 'sent_at', 'user_id',
     ];
 
+    /**
+     * @return HasMany<Link, $this>
+     */
     public function links(): HasMany
     {
         return $this->hasMany(Link::class);
     }
 
+    /**
+     * @return BelongsTo<User, $this>
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,19 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
      */
     protected $fillable = [
         'name',
@@ -25,8 +24,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -35,8 +32,6 @@ class User extends Authenticatable
 
     /**
      * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
      */
     protected function casts(): array
     {
